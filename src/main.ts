d5b77f378fc90a6b4f2dfd2893b9d6acfd8bf9fd
shahzaibalijamro <shahzaibalijamro@gmail.com>
2026-01-03 20:24:06 +0500

    vercel.json added

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,13 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({
     transform: true,
-    whitelist: true
+    whitelist: true,
+    forbidNonWhitelisted: true,
   }))
+  app.enableCors({
+    origin: process.env.CLIENT_URL,
+    credentials: true
+  })
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();

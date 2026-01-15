1103c7ac68e23ef2e8e2c2dbf0492290a57d8343
shahzaibalijamro <shahzaibalijamro@gmail.com>
2026-01-16 00:40:45 +0500

    Task service testing done

--- src/task/task.service.spec.ts
+++ src/task/task.service.spec.ts
@@ -1,8 +1,8 @@
 import { getRepositoryToken } from '@nestjs/typeorm';
-import { Task } from './task.entity';
+import { Status, Task } from './task.entity';
 import { TaskService } from './task.service';
 import { Test, TestingModule } from '@nestjs/testing';
-import { mockTasksArray, mockUser } from './mock.task';
+import { mockTask, mockTasksArray, mockUser } from './mock.task';
 import { Repository } from 'typeorm';
 import { NotFoundException } from '@nestjs/common';
 
@@ -14,6 +14,8 @@ describe('Tasks Service', () => {
     // Mock methods will be defined in each test
     createQueryBuilder: jest.fn(),
     findOne: jest.fn(),
+    save: jest.fn(),
+    remove: jest.fn(),
   };
 
   beforeEach(async () => {
@@ -46,9 +48,77 @@ describe('Tasks Service', () => {
         andWhere: jest.fn().mockReturnThis(),
         getMany: jest.fn().mockResolvedValue(mockTasksArray),
       };
+
+      mockTaskRepository.createQueryBuilder.mockReturnValue(mockQueryBuilder);
+
+      const result = await service.findAll(filters, mockUser);
+
+      expect(mockTaskRepository.createQueryBuilder).toHaveBeenCalledWith(
+        'task',
+      );
+
+      expect(mockQueryBuilder.andWhere).toHaveBeenCalledWith(
+        'task.userId = :id',
+        { id: mockUser.id },
+      );
+
+      expect(mockQueryBuilder.andWhere).toHaveBeenCalledTimes(1);
+
+      expect(result).toEqual(mockTasksArray);
+    });
+
+    it('should filter tasks by Status', async () => {
+      const filters = {
+        status: Status.IN_PROGRESS,
+      };
+      const mockQueryBuilder = {
+        andWhere: jest.fn().mockReturnThis(),
+        getMany: jest.fn().mockResolvedValue(mockTasksArray[1]),
+      };
       mockTaskRepository.createQueryBuilder.mockReturnValue(mockQueryBuilder);
       const result = await service.findAll(filters, mockUser);
-      expect(result).toEqual(mockTasksArray);
+      expect(mockTaskRepository.createQueryBuilder).toHaveBeenCalledWith(
+        'task',
+      );
+      expect(mockQueryBuilder.andWhere).toHaveBeenNthCalledWith(
+        1,
+        'task.userId = :id',
+        { id: mockUser.id },
+      );
+      expect(mockQueryBuilder.andWhere).toHaveBeenNthCalledWith(
+        2,
+        'task.status = :status',
+        { status: Status.IN_PROGRESS },
+      );
+      expect(mockQueryBuilder.andWhere).toHaveBeenCalledTimes(2);
+      expect(result).toEqual(mockTasksArray[1]);
+    });
+
+    it('should filter tasks by title or description', async () => {
+      const filters = {
+        search: 'Task 2',
+      };
+      const mockQueryBuilder = {
+        andWhere: jest.fn().mockReturnThis(),
+        getMany: jest.fn().mockResolvedValue(mockTasksArray[1]),
+      };
+      mockTaskRepository.createQueryBuilder.mockReturnValue(mockQueryBuilder);
+      const result = await service.findAll(filters, mockUser);
+      expect(mockTaskRepository.createQueryBuilder).toHaveBeenCalledWith(
+        'task',
+      );
+      expect(mockQueryBuilder.andWhere).toHaveBeenNthCalledWith(
+        1,
+        'task.userId = :id',
+        { id: mockUser.id },
+      );
+      expect(mockQueryBuilder.andWhere).toHaveBeenNthCalledWith(
+        2,
+        '(LOWER(task.title) LIKE :search OR LOWER(task.description) LIKE :search)',
+        { search: `%${'Task 2'.toLowerCase()}%` },
+      );
+      expect(mockQueryBuilder.andWhere).toHaveBeenCalledTimes(2);
+      expect(result).toEqual(mockTasksArray[1]);
     });
   });
 
@@ -61,7 +131,59 @@ describe('Tasks Service', () => {
     });
     it('should throw NotFoundException when task not found', async () => {
       mockTaskRepository.findOne.mockResolvedValue(null);
-      await expect(service.findTaskById(id, mockUser)).rejects.toThrow(NotFoundException);
+      await expect(service.findTaskById(id, mockUser)).rejects.toThrow(
+        NotFoundException,
+      );
+      await expect(service.findTaskById(id, mockUser)).rejects.toThrow(
+        `Task with id ${id} not found!`,
+      );
     });
   });
+
+  describe('Create Task method', () => {
+    const body = {
+      title: 'some task',
+      description: 'some description',
+    };
+    it('should create new task and return it', async () => {
+      mockTaskRepository.save.mockResolvedValue(mockTask);
+      const result = await service.createNewTask(body, mockUser);
+      expect(mockTaskRepository.save).toHaveBeenCalledWith({
+        title: 'some task',
+        description: 'some description',
+        status: Status.OPEN,
+        user: mockUser,
+      });
+      expect(result).toEqual(mockTask);
+    });
+  });
+
+  describe('Remove task by Id method', () => {
+    const id = 'task-id-123';
+    it('should delete the task by id and then return it', async () => {
+      mockTaskRepository.findOne.mockResolvedValue(mockTask);
+      const result = await service.removeTaskById(id, mockUser);
+      expect(mockTaskRepository.remove).toHaveBeenCalledTimes(1);
+      expect(mockTaskRepository.remove).toHaveBeenCalledWith(mockTask);
+      expect(result).toEqual(mockTask);
+    });
+  });
+
+
+  describe("Update Task By Id method", () => {
+    const id = 'task-id-123';
+
+    it("should update task status by Id", async () => {
+        mockTaskRepository.findOne.mockResolvedValue(mockTask);
+        const result = await service.updateTaskById({status: Status.DONE}, id, mockUser);
+        expect(mockTaskRepository.findOne).toHaveBeenCalledWith({
+            where: {
+                id,
+                user: mockUser,
+            }
+        });
+        expect(mockTaskRepository.save).toHaveBeenCalledWith(mockTask);
+        expect(result).toEqual(mockTask);
+    })
+  })
 });
